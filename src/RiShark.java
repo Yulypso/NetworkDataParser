230a2906153aef8c8299e60c994d41b50afeec40
Thierry KHAMPHOUSONE <khamphousone@et.Esiea.fr>
2021-10-06 23:19:17 +0200

    + PcapReader.java + RiShark.java

--- src/RiShark.java
+++ src/RiShark.java
@@ -1,6 +1,30 @@
+import java.io.FileReader;
+import java.util.Locale;
+
 public class RiShark {
 
     public static void main(String[] args) {
 
+        if (args.length != 1) {
+            System.out.println("Require: <filename>.pcap");
+            System.exit(-1);
+        }
+
+        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+            System.out.println(System.getProperty("user.dir") + "\\" + args[0]);
+        } else {
+            System.out.println(System.getProperty("user.dir") + "/" + args[0]);
+        }
+
+        /*try {
+            FileReader fileReader = new FileReader(args[0]);
+
+            int r = 0;
+            while((r = fileReader.read()) != -1) {
+                System.out.print((char) r);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
     }
 }
---
+++ src/readers/PcapReader.java
@@ -0,0 +1,8 @@
+package readers;
+
+public class PcapReader {
+
+    PcapReader(){
+
+    }
+}
